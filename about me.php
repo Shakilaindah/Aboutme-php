88e7256d13d5ad59aff86f9b2abd964396f6b98a
Shakila Indah Nurhasti <55771362+Shakilaindah@users.noreply.github.com>
2020-04-09 08:29:17 +0700

    Add files via upload

---
+++ about me.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Nama          : Shakila Indah Nurhasti <br>';
+echo 'Kelas         : XI RPL 1 <br>';
+echo 'Asal Sekolah  : SMK TARUNA BHAKTI <br>';
+echo 'Alamat        : Jl.Swadaya 2 no.03 Rt06/01 Tugu,Cimanggis Kota Depok <br>';
+
+
+?>
