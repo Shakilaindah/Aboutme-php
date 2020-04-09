cca14c1a1fc2a42236982aec507f22d3f422dc6b
Shakila Indah Nurhasti <55771362+Shakilaindah@users.noreply.github.com>
2020-04-09 09:27:42 +0700

    Update about me.php

--- about me.php
+++ about me.php
@@ -3,7 +3,8 @@
 echo 'Nama          : Shakila Indah Nurhasti <br>';
 echo 'Kelas         : XI RPL 1 <br>';
 echo 'Asal Sekolah  : SMK TARUNA BHAKTI <br>';
-echo 'Alamat        : Jl.Swadaya 2 no.03 Rt06/01 Tugu,Cimanggis Kota Depok <br>';
+echo 'HP            : 089638119907 <br>';
+echo 'Email         : shakilaindah27@gmail.com <br>';
 
 
 ?>
